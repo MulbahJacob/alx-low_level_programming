4d60a28ccdceccf9a1a4ad68e13a9fe09dfb22fb
Jacob Mulbah Wah <mulbahjacob837@gmail.com>
2023-10-17 07:15:31 -0700

    revstring

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
 void reset_to_98(int *n);
 int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+
+
+
 #endif
