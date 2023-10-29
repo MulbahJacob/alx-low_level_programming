2f6b3601ad2eeac94b21d34a5017d8a932d6dcf2
Jacob Mulbah Wah <mulbahjacob837@gmail.com>
2023-10-29 10:26:41 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * @ain - prinst program name
+ * main - prinst program name
  * @argc: argument count
  * @argv: arguments vector
  * Return: 0
@@ -9,6 +9,8 @@
 
 int main(int argc, char *argv[])
 {
+	(void) argc;
+
 	printf("%s\n", argv[0]);
 	return (0);
 
