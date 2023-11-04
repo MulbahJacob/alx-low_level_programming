1010ccce9f78f201987e0e688349507e6b8ea460
Jacob Mulbah Wah <mulbahjacob837@gmail.com>
2023-11-04 08:46:56 -0700

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,13 +11,14 @@
 
 int *array_range(int min, int max)
 {
-	int sie;
+	int i;
+	int size;
 	int *output;
 
 	if (min > max)
 		return (NULL);
 	size = (max - min) + 1;
-	output = malloc(sizeof(int) * size)
+	output = malloc(sizeof(int) * size);
 	if (output == NULL)
 		return (NULL);
 	for (i = 0; min <= max; i++)
