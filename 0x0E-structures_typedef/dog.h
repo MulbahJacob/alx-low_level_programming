d7cb390d1f6393208e750b44ad4a2d021cc56273
Jacob Mulbah Wah <mulbahjacob837@gmail.com>
2023-11-07 07:56:30 -0800

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - dog structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: nam of owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+
+
+#endif
